260039275d9c9d45088ac2826808cea7b7b11595
tim0618 <tim061801@gmail.com>
2025-09-30 18:18:47 +0800

    update: DB user table name change to users

--- MoneyPayBackend/DBContext.cs
+++ MoneyPayBackend/DBContext.cs
@@ -5,7 +5,7 @@ public class MoneyPayDBContext : DbContext
 {
     public MoneyPayDBContext(DbContextOptions<MoneyPayDBContext> options) : base(options) { }
 
-    public DbSet<UserModel> User { get; set; }
+    public DbSet<UserModel> Users { get; set; }
     public DbSet<MoneyTypesModel> MoneyTypes { get; set; }
     public DbSet<MoneyTypeDetailModel> MoneyTypeDetail { get; set; }
     public DbSet<MoneyTypeDetailRemarkModel> MoneyTypeDetailRemark { get; set; }
